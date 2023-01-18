a31e65c832faf69f17a4a18560b2c927c1f030de
Nicolas Colque <nicolascon93@gmail.com>
2023-01-18 15:33:24 -0400

    Creacion de los directorios components, interfaces, services y shared, cada uno con sus archivos base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { ListarMascotaComponent } from './components/listar-mascota/listar-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarEditarMascotaComponent,
+    ListarMascotaComponent,
+    VerMascotaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
