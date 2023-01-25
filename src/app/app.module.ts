12f4c5307fc953ecb76cc1702c6d18c8e7cd21c9
Nicolas Colque <nicolascon93@gmail.com>
2023-01-24 20:14:10 -0400

    Diseño de la interfaz para consumir datos del back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Componentes
 import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
 import { ListarMascotaComponent } from './components/listar-mascota/listar-mascota.component';
 import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material/material.module';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AgregarEditarMascotaComponent,
     ListarMascotaComponent,
-    VerMascotaComponent
+    VerMascotaComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
